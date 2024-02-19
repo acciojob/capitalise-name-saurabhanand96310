7a5dbbe116855940e47022459d4e701aad17082a
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-02-19 14:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 let p=document.querySelector("#fname");
-p.addEventListener("click", upperCase)
+p.addEventListener("blur", upperCase)
 function upperCase(e){
 let q=p.value.toUpperCase();
  p.value=q;
