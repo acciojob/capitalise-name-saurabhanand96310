fdfd46392e800c82a7652e2eb4ecbac41fd0b99b
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-02-19 14:43:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 //your JS code here. If required.
 let p=document.querySelector("#fname");
+p.addEventListener("blur" upperCase)
+function upperCase(e){
 let q=p.toUpperCase();
-console.log(q);
+return q ;
+	}
